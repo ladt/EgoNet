9ac9f660d9d621b9e669c0983b407075796d2ddd
elad <eladtalbi@gmail.com>
2021-10-05 15:53:02 +0300

    refactoring

--- tools/test_context.py
+++ tools/test_context.py
@@ -1,14 +1,31 @@
 import os
+from pathlib import Path
+import random
 
-
-
-
+def calc_new_box(img_arr, old_box):
+    pass
 
 def new_img(img_arr, old_box, new_box):
     pass
 
-def new_imgs(paths_in, out_dir, mode):
-    pass
+def new_imgs(in_dir, out_dir, mode, num_files):
+
+    # create sub directories - assuming no files in parent dir
+    for dir_name in os.listdir(in_dir):
+        new_dir_path = os.path.join(out_dir, dir_name)
+        Path(new_dir_path).mkdir(parents=True, exist_ok=True)
+
+    imgs_dirname = 'ImageSets'
+    # choose files randomly - assuming no dirs in subdirs
+    imgs_filenames = random.sample(os.listdir(os.path.join(in_dir, imgs_dirname)), num_files)
+
+    for img_filename in imgs_filenames:
+        img_src_path = os.path.join(in_dir, imgs_dirname, img_filename)
+        img_dst_path = os.path.join(out_dir, imgs_dirname, img_filename)
+        label_path = os.path.join(in_dir, 'label_2', img_filename)
+        old_box = choose_box(img_src_path, label_path)
+        new_box = calc_new_box(img_arr, old)
+
 
 def create_train_file(dir):
     # TODO sorted
@@ -23,10 +40,9 @@ if __name__=='__main__':
     stat_dir = '/home/elad/Data/KITTI/training_stat_blck'
     shift_dir= '/home/elad/Data/KITTI/training_shift_blck'
 
-    num_files = 10
+    in_dir = orig_dir
     out_dir = stat_dir
-    paths_in = [os.path.join(stat_dir, filename) for filename in os.listdir(orig_dir)[:num_files]]
-    new_imgs(paths_in=paths_in, out_dir=out_dir, mode='stat')
+    new_imgs(in_dir, out_dir, mode='stat', num_files=10)
     create_train_file(out_dir)
 
     # TODO run network
