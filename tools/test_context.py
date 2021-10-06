ab5eef113c856ba49c31cf485be18ae5b48df9a0
elad <eladtalbi@gmail.com>
2021-10-06 16:36:18 +0300

    refactoring

--- tools/test_context.py
+++ tools/test_context.py
@@ -12,13 +12,30 @@ def calc_new_box(img_path, old_box):
     pass
 
 def choose_line(img_pth, label_path):
-    # returns a line
+    # returns first line - where class is Car
     # if no match - return None
-    pass
+    img = Image.open(img_pth)
+    img_arr = np.array(img)
+    H, W, _ = img_arr.shape
+    with open(label_path, 'r') as src_file:
+        lines = src_file.read().splitlines()
+        for line in lines:
+            elems = line.split(' ')
+            if elems[0] != 'Car' or elems[0] != '0' or elems[0] != '0':
+                continue
+            left, top, right, bottom = [int(float(elem)) for elem in elems[4: 8]]
+            assert left >= 0 and right <= W-1 and top >= 0 and bottom <= H-1
+            return line
+    return None
 
 def new_img(img_arr, old_box, new_box):
     # returns a new image array
-    pass
+    new_img_arr = np.zeros_like(img_arr)
+    x1_old, y1_old, x2_old, y2_old = old_box
+    x1_new, y1_new, x2_new, y2_new = new_box
+    assert x2_new - x1_new == x2_old - x1_old and y2_new - y1_new == y2_old - y1_old
+    new_img_arr[y1_new: y2_new, x1_new: x2_new, :] = img_arr[y1_old: y2_old, x1_old : x2_old, :]
+    return new_img_arr
 
 def new_imgs(in_dir, out_dir, mode, num_files):
 
@@ -26,10 +43,13 @@ def new_imgs(in_dir, out_dir, mode, num_files):
     for dir_name in os.listdir(in_dir):
         Path(out_dir, dir_name).mkdir(parents=True, exist_ok=True)
 
+    imgs_filenames = os.listdir(Path(in_dir, 'ImageSets'))
     # choose files randomly - assuming no dirs in subdirs
-    imgs_filenames = random.sample(os.listdir(Path(in_dir, 'ImageSets')), num_files)
+    random.shuffle(imgs_filenames)
 
     for img_filename in imgs_filenames:
+
+        if num_files == 0: break
 
         img_src_path = Path(in_dir, 'ImageSets', img_filename)
         img_dst_path = Path(out_dir, 'ImageSets', img_filename)
@@ -37,7 +57,10 @@ def new_imgs(in_dir, out_dir, mode, num_files):
         label_dst_path = Path(out_dir, 'label_2', img_filename.split('.')[0] + '.txt')
 
         old_line = choose_line(img_src_path, label_src_path) # label line
-        if old_line is None: continue
+        if old_line is None:
+            continue
+        else:
+            num_files -= 1
         line_elemns = old_line.split(' ')
         old_box = [float(elem) for elem in line_elemns[4: 8]]
         new_box = calc_new_box(img_src_path, old_box) if mode == 'shift' else old_line # float list
@@ -62,7 +85,7 @@ def new_imgs(in_dir, out_dir, mode, num_files):
 
 def create_test_file(dir):
     file_path = Path(dir, 'ImageSets', 'test.txt')
-    file_names = sorted([file for file in os.listdir(str(Path(dir, 'image2'))) if file.endswith('.png')])
+    file_names = sorted([file for file in os.listdir(Path(dir, 'image2')) if file.endswith('.png')])
     with open(file_path, 'w') as test_file:
         for txt_filename in file_names:
             test_file.write(txt_filename.split('.')[0] + '\n')
@@ -71,10 +94,11 @@ def cmp_alpha(src, dst):
     pass
 
 def quant(src, dst):
+    # only cars
     Path(dst, 'calib').mkdir(parents=True, exist_ok=True)
     copy_tree(str(Path(src, 'calib')), str(Path(dst,'calib')))
     Path(dst, 'image2').mkdir(parents=True, exist_ok=True)
-    copy_tree(str(Path(src, 'image_2')), str(Path(dst, 'image2')))
+    copy_tree(str(Path(src, 'image_2')), str(Path(dst, 'image_2')))
     Path(dst, 'ImageSets').mkdir(parents=True, exist_ok=True)
     copy(str(Path(src, 'ImageSets', 'trainval.txt')), str(Path(dst, 'ImageSets')))
     shutil.move(str(Path(dst, 'ImageSets', 'trainval.txt')), str(Path(dst, 'ImageSets', 'test.txt')))
@@ -88,7 +112,7 @@ def quant(src, dst):
                 for line in lines:
                     elems = line.split(' ')
                     quant_box = ['{:3f}'.format(int(float(elem))) for elem in elems[4: 8]]
-                    new_line_list = [elems[0], *[-1] * 2, -10, *quant_box, *[-1] * 3,
+                    new_line_list = [elems[0], int(float(elems[1])), int(float(elems[2])), -10, *quant_box, *[-1] * 3,
                                      *[-1000] * 3, -10, '{:8f}'.format(0)]
                     new_line = (' ').join([str(elem) for elem in new_line_list])
                     dst_file.write(new_line + '\n')
@@ -112,6 +136,7 @@ if __name__=='__main__':
 
     # put all KITTI's quantizied GT data in new data folder - under testing
     # put KITTI's testing folder labels under 'resources'
+    # we don't filter only-cars labels: prediction is performed only for cars in images (images without cars - no output)
     kitti_orig_dir = '/home/elad/Data/KITTI/training'
     kitti_quant_dir = '/home/elad/Data/KITTI_QUANTIZIED/testing'
     quant(kitti_orig_dir, kitti_quant_dir)
@@ -130,10 +155,10 @@ if __name__=='__main__':
     copy_tree(str(Path(kitti_stat_dir, 'label_2')), str(Path('../resources', 'KITTI_STAT_test_boxes')))
     Path('../resources', 'KITTI_SHIFT_test_boxes').mkdir(parents=True, exist_ok=True)
     copy_tree(str(Path(kitti_shift_dir, 'label_2')), str(Path('../resources', 'KITTI_SHIFT_test_boxes')))
-    #
-    # # TODO run network
-    #
+
+    # TODO run network
+
     # orig_pred_dir = '/home/PycharmProjects/EgoNet/tools/YOUR_OUTPUT_DIR'
     # stat_pred_dir = '/home/PycharmProjects/EgoNet/tools/YOUR_STAT_DIR'
     # shift_pred_dir = '/home/PycharmProjects/EgoNet/tools/YOUR_SHIFT_DIR'
-    # cmp_alpha(orig_pred_dir, dst=stat_pred_dir) # TODO cmp alpha also between GT and pred for sanity check
+    # cmp_alpha(orig_pred_dir, dst=stat_pred_dir)
